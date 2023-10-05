5ca56453bccb60cbdb3e59091524f0990846277f
Jonas <jonasloop1@gmail.com>
2023-10-05 15:18:39 -0300

    Initial commit

---
+++ src/Tela.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Tela {
+    public Tela(){
+
+        JFrame jFrame = new JFrame();
+
+        jFrame.setVisible(true);
+        jFrame.setSize(800,500);
+        jFrame.setTitle("Calculadora");
+    }
+}
